afcc325213157028ba47904251c5d5a081b0e7e7
loujen <56408069+loujen@users.noreply.github.com>
2020-11-19 18:39:19 +0100

    add files

---
+++ torsdag1911b/src/Main.java
@@ -0,0 +1,23 @@
+    public class Main {
+
+        // 1. Queries in MySQL Workbench
+
+        //c. SELECT * FROM actor
+        //d. SELECT * FROM actor WHERE last_name = "Cage";
+        //e. SELECT * FROM ACTOR WHERE first_name != "Zero" OR first_name != "NICK";
+        //f. SELECT * FROM actor WHERE first_name IN ("Nick", "Johnny", "James", "Morgan", "Whoppi");
+        //g. SELECT * FROM actor WHERE actor_id between 50 AND 150;
+        //h. SELECT * FROM actor WHERE first_name LIKE "c%";
+        //i. SELECT * FROM actor ORDER BY first_name ASC;
+        //j. SELECT * FROM actor ORDER BY last_name DESC;
+        //k. SELECT COUNT(actor_id) FROM actor;
+        //l. SELECT COUNT(actor_id) FROM actor WHERE first_name = "nick";
+
+        // 2.
+        // a. INSERT INTO actor (first_name, last_name) VALUES ("Louise", "Jensen");
+        // c. UPDATE actor SET first_name = "Louise", last_name = "Rose" WHERE actor_id = 201;
+        // d. DELETE FROM actor WHERE actor_id = 201;
+
+    }
+
+}
